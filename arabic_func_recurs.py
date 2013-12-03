b803323bc4cb1f7a22428ee5d0c15f7e50c92323
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2013-12-03 22:04:43 +0200

    Create arabic_func_recurs.py

---
+++ arabic_func_recurs.py
@@ -0,0 +1,32 @@
+def rval(letr):
+	if(letr == "I"):
+		return 1
+	elif(letr == "V"):
+		return 5
+	elif(letr == "X"):
+		return 10
+	elif(letr == "L"):
+		return 50
+	elif(letr == "C"):
+		return 100
+	elif(letr == "D"):
+		return 500
+	elif(letr == "M"):
+		return 1000
+	else:
+		return "error"
+		
+		
+def arabic(n):
+    if len(n)==1: #First Base Case 
+        return rval(n)
+    elif len(n)==2:
+        if rval(n[0])>=rval(n[1]):
+	        return (rval(n[0])+rval(n[1]))
+        else:
+            return (rval(n[0])-rval(n[1]))
+    else:
+        if rval(n[len(n)-3]) > rval(n[len(n)-2]):
+            return arabic(n[len(n)-2:]) + arabic(n[:len(n)-2])
+        else:
+            return arabic(n[len(n)-1]) + arabic(n[:len(n)-1])
