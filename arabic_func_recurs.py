1b04601cbe4781dcd99e2666ee0581fd30594df1
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2013-12-03 22:08:32 +0200

    Fixed syntax.

--- arabic_func_recurs.py
+++ arabic_func_recurs.py
@@ -1,26 +1,28 @@
 def rval(letr):
-	if(letr == "I"):
-		return 1
-	elif(letr == "V"):
-		return 5
-	elif(letr == "X"):
-		return 10
-	elif(letr == "L"):
-		return 50
-	elif(letr == "C"):
-		return 100
-	elif(letr == "D"):
-		return 500
-	elif(letr == "M"):
-		return 1000
-	else:
-		return "error"
+    """Function translates one digit roman number to one digit arabic"""
+    if(letr == "I"):
+    	return 1
+    elif(letr == "V"):
+    	return 5
+    elif(letr == "X"):
+    	return 10
+    elif(letr == "L"):
+    	return 50
+    elif(letr == "C"):
+    	return 100
+    elif(letr == "D"):
+    	return 500
+    elif(letr == "M"):
+	return 1000
+    else:
+    	return "error"
 		
 		
 def arabic(n):
+    """Recursive function that translates roman number to arabic"""
     if len(n)==1: #First Base Case 
         return rval(n)
-    elif len(n)==2:
+    elif len(n)==2:#Second Base Case
         if rval(n[0])>=rval(n[1]):
 	        return (rval(n[0])+rval(n[1]))
         else:
