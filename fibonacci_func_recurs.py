4a4df9e8dfcaa302d17d80b55ad70393294c9f19
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2013-12-03 22:00:25 +0200

    Added description for the function fibonacci_func_recurs.py

--- fibonacci_func_recurs.py
+++ fibonacci_func_recurs.py
@@ -1,7 +1,8 @@
 def fibonacci(n):
-	if(n == 1):
-		return 0#Base case 1
-	elif(n == 2):
-		return 1#Base case 2 
-	else:
-		return fibonacci(n-1)+fibonacci(n-2)
+    """Recursion function that returns fibonacci number by entering fibonacci index of the number"""
+    if(n == 1):
+    	return 0#Base case 1
+    elif(n == 2):
+    	return 1#Base case 2 
+    else:
+    	return fibonacci(n-1)+fibonacci(n-2)
