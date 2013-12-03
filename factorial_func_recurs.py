705ce30e00dd7092780c13875579c804644a915c
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2013-12-03 21:50:57 +0200

    Added small comment

--- factorial_func_recurs.py
+++ factorial_func_recurs.py
@@ -1,6 +1,6 @@
 def factorial(n):
     if(n == 0):
     """Recursion function that returns the factorial of the number"""
-		return 1#What was our base case? 
+		return 1#this is our base case? 
 	else:
 		return n*factorial(n-1)
