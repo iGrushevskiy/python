34aac48700f845acf66474cff1700e955a86c411
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2014-02-04 17:17:40 +0200

    Update grocery_list.py

--- grocery_list.py
+++ grocery_list.py
@@ -13,7 +13,22 @@ stock={
 }
 
 
+#calculates total price for all items in the stock
 for item in prices and stock:
     print item #Prints the name
     print "price: " + str(prices[item]) #Prints the price
     print "stock: " + str(stock[item]) #Prints the stock
+
+#here is the way to calculate your exect shopping list
+
+#your personal shopping list
+shopping_list = ["banana", "orange", "apple"]
+
+#function which calculates total price for the items in your list
+def compute_bill(food):
+    total=0
+    for item in food:
+        total=total+prices[item]
+    return total
+
+print compute_bill(shopping_list)
